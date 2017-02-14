3ad6b6dafd282bb0fce33afb55de718bbd41d319
Dmitry [KP0H] Pelevin <dmitry@pelevin.pro>
2017-02-15 00:14:49 +0300

    Add OrganizationServiceExtensions

---
+++ Extensions/OrganizationServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xrm.Sdk;
+
+namespace KpdApps.Common.MsCrm2013.Extensions
+{
+	/// <summary>
+	/// Extensions for <see cref="IOrganizationService"/>.
+	/// </summary>
+	public static class OrganizationServiceExtensions
+	{
+		
+	}
+}
