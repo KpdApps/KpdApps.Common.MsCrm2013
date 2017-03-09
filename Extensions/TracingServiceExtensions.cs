ae92c9aec91ba101d2b15fbe9165ad3f7f4db13f
Dmitriy Pelevin <dmitry@pelevin.pro>
2017-03-09 12:37:51 +0300

    Add TracingServiceExtensions

---
+++ Extensions/TracingServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace KpdApps.Common.MsCrm2013.Extensions
+{
+    public static class TracingServiceExtensions
+    {
+        public static void TraceError(this ITracingService tracingService, Exception exception)
+        {
+            tracingService.Trace($"[Error] {exception.Source}:{exception.Message}\r\n{exception.StackTrace}\r\n");
+            if (exception.InnerException != null)
+                tracingService.TraceError(exception.InnerException);
+        }
+
+        public static void TraceWarning(this ITracingService tracingService, string format, params object[] args)
+        {
+            tracingService.Trace($"[Warning] {string.Format(format, args)}");
+        }
+
+        public static void TraceInfo(this ITracingService tracingService, string format, params object[] args)
+        {
+            tracingService.Trace($"[Info] {string.Format(format, args)}");
+        }
+    }
+}
