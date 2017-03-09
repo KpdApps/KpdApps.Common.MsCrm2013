ef71c98a96991f0924abffc3038766495019ca3b
Dmitriy Pelevin <dmitry@pelevin.pro>
2017-03-09 12:42:00 +0300

    Add BasePluginAction

---
+++ BasePluginAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+
+namespace KpdApps.Common.MsCrm2013
+{
+    public class BasePluginAction
+    {
+        public PluginState State { get; set; }
+
+        public IOrganizationService Service => State.Service;
+
+        public BasePluginAction(PluginState state)
+        {
+            State = state;
+        }
+
+        public virtual void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
